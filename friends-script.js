ca4774fae6cc453c0e2b73095f9f2424293c4fe2
Ben Mahowald <ben.mahowald@gmail.com>
2017-01-09 16:48:45 -0600

    loop to unveil all anchors on friends page in cascade appearance

---
+++ friends-script.js
@@ -0,0 +1,20 @@
+console.log('in friends-script');
+
+$(document).ready( function () {
+  console.log('jq sourced');
+
+  // find link-list in DOM and count all anchor tags
+  var content = document.getElementById('link-list');
+  var divAnchors = content.getElementsByTagName('a');
+
+  // loop through all anchors and add animation class of 'unveil'
+    for(var i = 0; i < divAnchors.length; i++) {
+
+      (function(i){
+        setTimeout(function(){
+            $(divAnchors[i]).animate({opacity: '1.0'}).fadeIn(1000);
+        }, 200 * i);
+      }(i)); // end wrapped function(i)
+    
+    } // end for loop
+}); // end jq ready function
